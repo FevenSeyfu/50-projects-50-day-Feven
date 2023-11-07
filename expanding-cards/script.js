e9317868a1f6689693f6f4f320e724ed8da45f17
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-08 02:02:09 +0300

    add readme

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -3,14 +3,14 @@ let cards = document.getElementsByClassName('card');
 const removeExpand= () => {
     for (const card of cards){
         card.classList.remove('expand');
-        card.childNodes[1].style.visibility = 'hidden';
+        card.childNodes[1].classList.remove('show');
     };
 }
 for (const card of cards){
     card.addEventListener("click",(e) =>{
         removeExpand();
         e.target.classList.add('expand');
-        e.target.childNodes[1].style.visibility = 'visible';
+        e.target.childNodes[1].classList.add('show');
     });
 }
 
