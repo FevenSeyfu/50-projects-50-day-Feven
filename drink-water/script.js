fe7e83d2d031102d89d6b6b0719f348057725d19
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-26 22:20:06 +0300

    Update status on big cup

--- drink-water/script.js
+++ drink-water/script.js
@@ -3,15 +3,32 @@ const literSpan =  document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
+function updateBigCup(){
+    const fullCups =  document.querySelectorAll('.cup-small.full').length;
+    const totalCups =  smallCups.length;
+    
+    if(fullCups === 0){
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;
+    }else{
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${fullCups /totalCups *330}px`;
+        percentage.innerText = `${fullCups /totalCups *100}%`;
+    }
+
+    if( fullCups === totalCups){
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
+    }else{
+        remained.style.visibility = 'visible';
+        literSpan.innerText = `${2 - (250 * fullCups  /1000)}L`;
+    }
+}
 updateBigCup()
 
 smallCups.forEach((cup,idx)=>{
     cup.addEventListener('click',()=>highlightCup(idx))
 })
-
-function updateBigCup(){
-
-}
 
 const highlightCup = (idx) =>{
     if(idx === 7 && smallCups[idx].classList.contains('full')) idx--;
