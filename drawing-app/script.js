13deadbaf21e001d4646a075cccda6594475346b
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-03 22:51:50 +0300

    complete drawing app

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -26,6 +26,7 @@ document.addEventListener('mouseup', (e) => {
     x = undefined
     y = undefined
 })
+// function to draw circle and line
 canvas.addEventListener('mousemove', (e) => {
     if(isPressed) {
         const x2 = e.offsetX
@@ -38,6 +39,21 @@ canvas.addEventListener('mousemove', (e) => {
         y = y2
     }
 })
+
+function drawCircle(x,y){
+    context.beginPath();
+    context.arc(x, y, size, 0, Math.PI * 2)
+    context.fillStyle = color
+    context.fill()
+}
+function drawLine(x1,y1,x2,y2){
+    context.beginPath()
+    context.moveTo(x1, y1)
+    context.lineTo(x2, y2)
+    context.strokeStyle = color
+    context.lineWidth = size * 2
+    context.stroke()
+}
 
 const updateSizeOnScreen=() => {
     sizeElt.innerText = size
