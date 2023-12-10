388b3cd1a94404e215cfeb11b6a05921d3c750a9
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-11 00:01:53 +0300

    fix link demo

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ projects.forEach(project=>{
         <div class="overlay">
             <h2 class="project-name">${project.name}</h2>
             <button class="btn">
-                <a href="/${project.name}/">Live Demo</a>
+                <a href="https://fevenseyfu.github.io/50-projects-50-day-Feven/${project.name}/">Live Demo</a>
             </button>
         </div>
             `
